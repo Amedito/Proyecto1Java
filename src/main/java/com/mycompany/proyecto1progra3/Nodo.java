fdcd69c397e602a0a0fca481a87785d29e28a8ac
Amedito <42658963+Amedito@users.noreply.github.com>
2023-03-10 21:43:29 -0600

    Update Nodo.java

--- src/main/java/com/mycompany/proyecto1progra3/Nodo.java
+++ src/main/java/com/mycompany/proyecto1progra3/Nodo.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.proyecto1progra3;
-
-/**
- *
- * @author William Carrillo
- */
-public class Nodo {
-    
+public class Nodos {
+	private String informacion;
+	private Nodos padre;
+	private Nodos nodoIzquierdo;
+	private Nodos nodoDerecho;
+	
+	public Nodos(){
+		this.informacion = "";
+		this.nodoIzquierdo = null;
+		this.nodoDerecho = null;
+		this.padre = null;
+	}
+	
+	public Nodos(String informacion) {
+		this.informacion = informacion;
+		this.nodoIzquierdo = null;
+		this.nodoDerecho = null;
+		this.padre = null;
+	}
+	
+	public Nodos (Nodos op1, String pop, Nodos op2){
+		this.informacion = pop;
+		this.nodoIzquierdo = op2;
+		this.nodoDerecho = op1;
+		this.padre = null;
+	}
+	
+	public Nodos getPadre() {
+		return padre;
+	}
+	
+	public void setPadre(Nodos padre){
+		this.padre = padre;
+	}
+	
+	public String getInformacion(){
+		return informacion;
+	}
+	
+	public void setInformacion(String informacion){
+		this.informacion = informacion;
+	}
+	
+	public Nodos getNodoIzquierdo() {
+		return nodoIzquierdo;
+	}
+	
+	public void setNodoIzquierdo(Nodos nodoIzquierdo){
+		this.nodoIzquierdo = nodoIzquierdo;
+	}
+	
+	public Nodos getNodoDerecho(){
+		return nodoDerecho;
+	}
+	
+	public void setNodoDerecho(Nodos nodoDerecho){
+		this.nodoDerecho = nodoDerecho;
+	}
 }
