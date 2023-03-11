014fe5544dcb603a880e1983f2ac4fcaec9a360c
wcarrillo1 <wcarrillo@isbelasoft.com>
2023-03-10 21:42:22 -0600

    First commit

---
+++ src/main/java/com/mycompany/proyecto1progra3/Proyecto1Progra3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1progra3;
+
+/**
+ *
+ * @author William Carrillo
+ */
+public class Proyecto1Progra3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
